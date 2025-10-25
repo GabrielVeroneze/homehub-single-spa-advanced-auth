b574ebf13a8a6c426c24e529840efb645758328a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-25 16:03:10 -0300

    ✨ feat: integrar função helloWorld do módulo @homehub/react-utils no App
    Adiciona a chamada da função helloWorld importada de @homehub/react-utils dentro do componente App. Garante a integração inicial entre o microfrontend de login e o módulo compartilhado react-utils.

--- react-login/src/App.tsx
+++ react-login/src/App.tsx
@@ -1,8 +1,11 @@
 import { Box } from '@mui/material'
+import { helloWorld } from '@homehub/react-utils'
 import LoginBox from './components/LoginBox'
 import backgroundImg from './assets/background.png'
 
 const App = () => {
+    helloWorld()
+
     return (
         <Box
             sx={{
