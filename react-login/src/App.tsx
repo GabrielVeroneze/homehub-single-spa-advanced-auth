07f030aab04f0a5f5a64e9a636ba743de314846c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-26 13:54:28 -0300

    :broom: cleanup: remover função de teste helloWorld do App
    Remove a chamada da função helloWorld do componente App, mantendo apenas a renderização da interface de login.

--- react-login/src/App.tsx
+++ react-login/src/App.tsx
@@ -1,11 +1,8 @@
 import { Box } from '@mui/material'
-import { helloWorld } from '@homehub/react-utils'
 import LoginBox from './components/LoginBox'
 import backgroundImg from './assets/background.png'
 
 const App = () => {
-    helloWorld()
-
     return (
         <Box
             sx={{
