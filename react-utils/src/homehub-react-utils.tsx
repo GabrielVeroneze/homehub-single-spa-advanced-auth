f040e53a1c14902cb59a8b3cfdce92c13884da97
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-26 13:52:45 -0300

    ✨ feat: adiciona loginFunction com persistência de autenticação via cookies
    Substitui a função helloWorld pela implementação de loginFunction, responsável por gerar um authId, salvar dados de autenticação em cookies e redirecionar o usuário para o dashboard correspondente.

--- react-utils/src/homehub-react-utils.tsx
+++ react-utils/src/homehub-react-utils.tsx
@@ -1,4 +1,11 @@
-// Anything exported from this file is importable by other in-browser modules.
-export function helloWorld() {
-    console.log('Hello, World!')
+import { AuthInfo } from './types/AuthInfo'
+import Cookies from 'universal-cookie'
+
+export const loginFunction = (email: AuthInfo['email']) => {
+    const authId = crypto.randomUUID()
+    const cookies = new Cookies(null, { path: '/' })
+
+    cookies.set('auth', { email: email, authId: authId })
+
+    location.replace(`/dashboard/${authId}/`)
 }
