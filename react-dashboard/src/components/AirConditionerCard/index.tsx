e3299b667471417a11f614ffcb9ce3acf7d2d24b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-28 17:52:01 -0300

    ✨ feat: aprimora AirConditionerCard com controle de temperatura e navegação
    Atualiza o componente AirConditionerCard adicionando controles interativos, como Slider de temperatura, botão de ligar/desligar e navegação com MobileStepper.

--- react-dashboard/src/components/AirConditionerCard/index.tsx
+++ react-dashboard/src/components/AirConditionerCard/index.tsx
@@ -1,40 +1,134 @@
-import { Box, Card, CardContent, Slider } from '@mui/material'
+import { useState } from 'react'
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    MobileStepper,
+    Slider,
+    Typography,
+} from '@mui/material'
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material'
 
 const marks = [
     {
-        value: 0,
-        label: '0°C',
+        value: 16,
+        label: '16°C',
     },
     {
-        value: 20,
-        label: '20°C',
-    },
-    {
-        value: 37,
-        label: '37°C',
-    },
-    {
-        value: 100,
-        label: '100°C',
+        value: 27,
+        label: '27°C',
     },
 ]
 
-function valuetext(value: number) {
+const valuetext = (value: number) => {
     return `${value}°C`
 }
 
 const AirConditionerCard = () => {
+    const [activeStep, setActiveStep] = useState(0)
+    const maxSteps = 4
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1)
+    }
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1)
+    }
+
     return (
         <Card sx={{ backgroundColor: '#F5F5F5', height: '100%' }}>
-            <CardContent>
-                <Box sx={{ width: 300 }}>
+            <CardContent
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 3,
+                    px: 2,
+                    py: 3,
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Typography>Sala de TV</Typography>
+                    <Button
+                        variant="outlined"
+                        size="large"
+                        sx={{ color: '#9C27B0', borderColor: '#9C27B0' }}
+                    >
+                        Desligar
+                    </Button>
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography>Temperatura</Typography>
+                    <Typography sx={{ fontSize: 34, lineHeight: 1.24, mb: 2 }}>
+                        23º
+                    </Typography>
                     <Slider
-                        aria-label="Custom marks"
                         defaultValue={20}
                         getAriaValueText={valuetext}
-                        step={10}
                         valueLabelDisplay="auto"
                         marks={marks}
+                        min={16}
+                        max={27}
+                        size="medium"
+                        sx={{
+                            color: '#E65100',
+                            '& .MuiSlider-markLabel[data-index="0"]': {
+                                transform: 'translateX(0)',
+                            },
+                            '& .MuiSlider-markLabel[data-index="1"]': {
+                                transform: 'translateX(-100%)',
+                            },
+                        }}
+                    />
+                </Box>
+                <Box>
+                    <MobileStepper
+                        variant="text"
+                        steps={maxSteps}
+                        position="static"
+                        activeStep={activeStep}
+                        sx={{
+                            fontFamily: 'Roboto',
+                            bgcolor: 'transparent',
+                            px: 0,
+                            py: 0.75,
+                            mt: 2,
+                        }}
+                        nextButton={
+                            <Button
+                                size="small"
+                                onClick={handleNext}
+                                disabled={activeStep === maxSteps - 1}
+                                sx={{ color: '#9C27B0' }}
+                            >
+                                próximo
+                                <KeyboardArrowRight />
+                            </Button>
+                        }
+                        backButton={
+                            <Button
+                                size="small"
+                                onClick={handleBack}
+                                disabled={activeStep === 0}
+                                sx={{ color: '#9C27B0' }}
+                            >
+                                <KeyboardArrowLeft />
+                                voltar
+                            </Button>
+                        }
                     />
                 </Box>
             </CardContent>
